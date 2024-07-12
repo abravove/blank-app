350d7713fb77e6d3ab5e8c8d4287afdfa65423e9
abravove <103659534+abravove@users.noreply.github.com>
2024-07-12 19:02:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,27 @@ def main():
     st.title('Línea de Tiempo')
 
     # Paso 1
-    with st.beta_expander("Paso 1: Descripción del paso 1"):
+    with st.expander("Paso 1: Descripción del paso 1"):
         st.write("Detalles del paso 1")
 
     # Paso 2
-    with st.beta_expander("Paso 2: Descripción del paso 2"):
+    with st.expander("Paso 2: Descripción del paso 2"):
         st.write("Detalles del paso 2")
 
     # Paso 3
-    with st.beta_expander("Paso 3: Descripción del paso 3"):
+    with st.expander("Paso 3: Descripción del paso 3"):
         st.write("Detalles del paso 3")
 
     # Paso 4
-    with st.beta_expander("Paso 4: Descripción del paso 4"):
+    with st.expander("Paso 4: Descripción del paso 4"):
         st.write("Detalles del paso 4")
 
     # Paso 5
-    with st.beta_expander("Paso 5: Descripción del paso 5"):
+    with st.expander("Paso 5: Descripción del paso 5"):
         st.write("Detalles del paso 5")
 
     # Paso 6
-    with st.beta_expander("Paso 6: Descripción del paso 6"):
+    with st.expander("Paso 6: Descripción del paso 6"):
         st.write("Detalles del paso 6")
 
 if __name__ == '__main__':
